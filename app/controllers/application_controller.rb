6bdb079e922f379dfb1e29ce936a890f6f256cda
sachin <devsachin108@gmail.com>
2018-02-14 20:09:21 +0530

    Make raise not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception  
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_teacher!
+  rescue_from ActionController::UnknownFormat, with: :raise_not_found
 
   protected
-
-
+  
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
 	end
-
+	
+  def raise_not_found
+    redirect_to root_path
+  end
+  
 end
