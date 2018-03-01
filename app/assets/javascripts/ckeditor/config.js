0a60c9b2319cd9e6e9c6501a1fd31dbdde238631
sachin <devsachin108@gmail.com>
2018-03-01 15:10:05 +0530

    add some css changes

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,11 +2,20 @@ CKEDITOR.editorConfig = function (config) {
   // ... other configuration ...
 
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline", "Font", "FontSize" ],
+    ["Bold",  "Italic",  "Underline", "Font"],
   ];
   config.toolbar = "mini";
+  config.height = 100;        // 500 pixels high.
+	config.height = '5em';
+	config.removePlugins = 'elementspath';
+config.resize_enabled = false;
 
   // ... rest of the original config.js  ...
 }
 
 CKEDITOR.config.autoParagraph = false;
+
+CKEDITOR.config.nomargin_style = {
+    element: ['body'],
+    styles: { 'margin': '0'}
+};
