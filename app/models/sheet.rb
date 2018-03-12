a0f32577c680a5db7235eb7458ce07bc97c3c284
sachin <devsachin108@gmail.com>
2018-03-12 13:29:51 +0530

    Fixed formating issue for comment that appears on report

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -11,4 +11,8 @@ class Sheet < ApplicationRecord
   def self.get_details(id)
     find_by_sql("select children.name, sheets.title, grades.abr as grade_name, grades.color, details.comment from subjects INNER JOIN students ON subjects.id = students.subject_id and subjects.id = #{id} INNER JOIN children ON children.id = students.child_id INNER JOIN details ON details.child_id = children.id INNER JOIN sheets ON sheets.id = details.sheet_id FULL JOIN grades ON grades.id = details.grade_id where sheets.subject_id = #{id} order by children.name,sheets.title, grade_name, details.comment")
   end
+
+  def sanitize_comment
+    comment? ? ActionView::Base.full_sanitizer.sanitize(comment) : ''   
+  end
 end
