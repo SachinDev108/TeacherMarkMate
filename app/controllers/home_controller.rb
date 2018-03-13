f639b4de2b875c94369b16f68f2f03001db9f0ed
sachin <devsachin108@gmail.com>
2018-03-13 15:25:12 +0530

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   before_action :authenticate_teacher! , :except => [:subscription]
   def index
-  	@subjects = current_teacher.subjects
-  	@children = current_teacher.children
+    @subjects = current_teacher.subjects
+    @children = current_teacher.children
+    @details_count = current_teacher.details.updated_in_between((Time.zone.now - 30.days), Time.zone.now).count
   end
 
   def subscription
--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,7 +2,10 @@ class Detail < ApplicationRecord
   belongs_to :sheet
   belongs_to :child
 
+  #scope
+  scope :updated_in_between, -> (start_at, end_at) { where(updated_at: (start_at)..(end_at)) }
+
   def grade
-  	Grade.find_by_id(grade_id)
+    Grade.find_by_id(grade_id)
   end
 end
