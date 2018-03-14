843396c8e4c759ae62573554e1cc54584985d3db
sachin <devsachin108@gmail.com>
2018-03-14 09:29:45 +0530

    changed payment mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 # Admin 
 Admin.create!(email: 'admin@teachermarkmate.com', password: 'admin@123')
 
-SubscriptionType.create(name: "Individual Plan", description: '', price: '19.99', yearly_price: '239.88', no_of_printers: '1', sort_order: '1')
+SubscriptionType.create(name: "Individual Plan", description: '', price: '19.99', yearly_price: '199', printer_price: '39.99', sort_order: '1')
 
-SubscriptionType.create(name: "5 Users Plan", description: '', price: '69.99', yearly_price: '839.88', no_of_printers: '1', sort_order: '2')
+SubscriptionType.create(name: "5 Users Plan", description: '', price: '69.99', yearly_price: '189', printer_price: '39.99', sort_order: '2')
 
-SubscriptionType.create(name: "10 Users Plan", description: '', price: '129.99', yearly_price: '1559.88', no_of_printers: '1', sort_order: '3')
+SubscriptionType.create(name: "10 Users Plan", description: '', price: '129.99', yearly_price: '159', printer_price: '39.99', sort_order: '3')
 
-SubscriptionType.create(name: "20 Users Plan", description: '', price: '249.99', yearly_price: '2999.88', no_of_printers: '1', sort_order: '4')
+SubscriptionType.create(name: "20 Users Plan", description: '', price: '249.99', yearly_price: '139', printer_price: '39.99', sort_order: '4')
