c969f7c9eb0a76635f25bdc76d44d08f9100235b
sachin <devsachin108@gmail.com>
2018-03-15 20:47:10 +0530

    quick fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < Devise::SessionsController
 
   def check_subcription
     teacher = Teacher.find_by_email(params[:teacher][:email])
-    unless (teacher && teacher.plan && teacher.plan.status) || teacher.is_teacher?
+    unless (teacher && teacher.plan && teacher.plan.status) || teacher.try(:is_teacher?)
       flash[:notice] = "Please sign up first"
       redirect_to new_teacher_registration_path
     end
