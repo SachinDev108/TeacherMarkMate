52fe6663699028c2a92f146577dc5429eefc9b8c
sachin <devsachin108@gmail.com>
2018-03-16 15:10:04 +0530

    Teacher permission

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,23 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new                # guest user (not logged in)
-    if user && (user.is_head?)
-      can :manage, Teacher, parent_id: user.id
+    if user.present?
+      if user.is_plan_active?
+        if user.is_head?
+          can :manage, Teacher, parent_id: user.id
+        end
+        can :manage, Child, teacher_id: user.id
+        can :manage, Subject, teacher_id: user.id
+        can :manage, Sheet, teacher_id: user.id
+      else
+        if user.is_head?
+          can :read, Teacher, parent_id: user.id
+        end
+        can :read, Child, teacher_id: user.id
+        can :read, Subject, teacher_id: user.id
+        can :read, Sheet, teacher_id: user.id
+        can [:report, :report_details], Sheet, teacher_id: user.id
+      end
     end
-    can :manage, Child, teacher_id: user.id
-    can :manage, Subject, teacher_id: user.id
-    can :manage, Sheet, teacher_id: user.id
   end
 end
