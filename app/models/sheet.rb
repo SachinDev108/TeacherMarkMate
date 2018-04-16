379fb369a0cac44797c4323582909a5c9347dae7
sachin <devsachin108@gmail.com>
2018-04-16 15:37:20 +0530

    Fix sheets order and markbook width issue

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -9,10 +9,10 @@ class Sheet < ApplicationRecord
 
 
   def self.get_details(id)
-    find_by_sql("select children.name, sheets.title, grades.abr as grade_name, grades.color, details.comment from subjects INNER JOIN students ON subjects.id = students.subject_id and subjects.id = #{id} INNER JOIN children ON children.id = students.child_id INNER JOIN details ON details.child_id = children.id INNER JOIN sheets ON sheets.id = details.sheet_id FULL JOIN grades ON grades.id = details.grade_id where sheets.subject_id = #{id} order by children.name,sheets.title, grade_name, details.comment")
+    find_by_sql("select children.name, sheets.title, grades.abr as grade_name, grades.color, details.comment from subjects INNER JOIN students ON subjects.id = students.subject_id and subjects.id = #{id} INNER JOIN children ON children.id = students.child_id INNER JOIN details ON details.child_id = children.id INNER JOIN sheets ON sheets.id = details.sheet_id FULL JOIN grades ON grades.id = details.grade_id where sheets.subject_id = #{id} order by children.name,grade_name, details.comment, sheets.created_at DESC")
   end
 
   def sanitize_comment
-    comment? ? ActionView::Base.full_sanitizer.sanitize(comment) : ''   
+    comment? ? ActionView::Base.full_sanitizer.sanitize(comment) : ''
   end
 end
