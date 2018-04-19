2f1c807f0903733f57cf298b513d78149c653aed
ashok <ashoks.heyday@gmail.com>
2018-04-19 17:24:08 +0530

    fixed report absent grade name

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,6 +5,5 @@ class Subject < ApplicationRecord
   has_many :sheets, :dependent => :destroy
   belongs_to :teacher
   accepts_nested_attributes_for :grades, allow_destroy: true
-
   validates :name, :teacher_id, :grades, presence: true
 end
