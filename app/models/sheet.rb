3d10a3a9ac14b820770a7cf5cc0da31feaf85e40
ashok <ashoks.heyday@gmail.com>
2018-04-19 18:08:46 +0530

    add changes on sheet model

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -9,7 +9,7 @@ class Sheet < ApplicationRecord
 
 
   def self.get_details(id)
-    find_by_sql("select children.name, sheets.title, grades.abr as grade_name, grades.color, details.comment from subjects INNER JOIN students ON subjects.id = students.subject_id and subjects.id = #{id} INNER JOIN children ON children.id = students.child_id INNER JOIN details ON details.child_id = children.id INNER JOIN sheets ON sheets.id = details.sheet_id FULL JOIN grades ON grades.id = details.grade_id where sheets.subject_id = #{id} order by sheets.created_at DESC, children.name,grade_name, details.comment")
+    find_by_sql("select children.id as children_id, children.name, sheets.title, grades.abr as grade_name, grades.color, details.comment from subjects INNER JOIN students ON subjects.id = students.subject_id and subjects.id = #{id} INNER JOIN children ON children.id = students.child_id INNER JOIN details ON details.child_id = children.id INNER JOIN sheets ON sheets.id = details.sheet_id FULL JOIN grades ON grades.id = details.grade_id where sheets.subject_id = #{id} order by sheets.created_at DESC, children.name,grade_name, details.comment")
   end
 
   def sanitize_comment
